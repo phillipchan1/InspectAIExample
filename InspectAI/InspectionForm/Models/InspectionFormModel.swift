9cd1ed5146ffba0d7bdf0ef5da95716ef6587c72
phillipchan1 <phillipchan1@gmail.com>
2023-07-06 15:22:07 -0700

    Updated Structure to new design

---
+++ InspectAI/InspectionForm/Models/InspectionFormModel.swift
@@ -0,0 +1,17 @@
+//
+//  InspectionFormModel.swift
+//  InspectAIExample
+//
+//  Created by Phil Chan on 7/6/23.
+//
+
+import Foundation
+
+
+class InspectionFormModel {
+    
+    submit() {
+        // do nothing
+    }
+    
+}
---
+++ InspectAI/InspectionForm/Views/InspectionForm.swift
@@ -0,0 +1,20 @@
+//
+//  InspectionForm.swift
+//  InspectAIExample
+//
+//  Created by Phil Chan on 7/6/23.
+//
+
+import SwiftUI
+
+struct InspectionForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InspectionForm_Previews: PreviewProvider {
+    static var previews: some View {
+        InspectionForm()
+    }
+}
